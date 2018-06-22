607693fda883bf810b5216233a2b2339455e5fc2
Anupom Roy <anupomr@yahoo.com>
2018-06-22 16:05:00 -0400

    Entity Framework with database First

---
+++ ConsoleAppDBFrst/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDBFrst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                // Create and save a new Blog 
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+
+                Console.Write("Enter a new Url: ");
+                var urlString = Console.ReadLine();
+
+                var blog = new Blog { Name = name, Url = urlString };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                // Display all Blogs from the database 
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine($"Blog : {item.Name} URL:{item.Url}");
+                }
+
+              
+                
+            }
+        }
+    }
+}
---
+++ DBFirstSample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContextAr())
+            {
+                // Create and save a new Blog 
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+                Console.Write("Enter a new Url: ");
+                var urlString = Console.ReadLine();
+
+                var blog = new Blog { Name = name, Url= urlString };
+                
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                // Display all Blogs from the database 
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine($"Blog : {item.Name} URL:{item.Url}");
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
